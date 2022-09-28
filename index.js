3d97ff3b269d2f9b2f8316512fa01168ae08ab59
Ben Newman <ben.newman52@ymail.com>
2022-09-27 17:51:21 -0700

    uhhh  idek where im at

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const character = newImage('assets/green-character/static.gif')
 move(character).to(100, 250)
 
 
+
 move(newImage('assets/tree.png')).to(200, 450)
 move(newImage('assets/pillar.png')).to(350, 250)
 move(newImage('assets/pine-tree.png')).to(450, 350)
@@ -13,3 +14,4 @@ move(newImage('assets/well.png')).to(500, 575)
 move(newItem('assets/sword.png')).to(500, 555)
 move(newItem('assets/shield.png')).to(165, 335)
 move(newItem('assets/staff.png')).to(600, 250)
+
